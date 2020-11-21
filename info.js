3df07cacc56e317788ff60791cf07eb824447982
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2020-11-21 21:39:49 +0500

    Update info.js

--- info.js
+++ info.js
@@ -1,8 +1,10 @@
+ var charNow = ['https://uploadstatic-sea.mihoyo.com/contentweb/20200903/2020090319230398794.png'];
+ 
+
 let openinfo = {
         launcherVer: "beta 0.2",
         launcherAuthor: "TheFunnyDay",
         launcherName: "Genshin Launcher (Unofficial)",
         bgNow: "https://images5.alphacoders.com/109/thumb-1920-1099191.jpg",
-        charNow: "https://uploadstatic-sea.mihoyo.com/contentweb/20200903/2020090319230398794.png",
 };
 
