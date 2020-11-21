e0c7db4c9e18ac1447f84d81fd1153178479e961
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2020-11-21 20:35:01 +0500

    Create info.js

---
+++ info.js
@@ -0,0 +1,8 @@
+let openinfo = {
+        launcherVer: "beta 0.2",
+        launcherAuthor: "TheFunnyDay",
+        launcherName: "JSR Launcher",
+        bgNow: "https://i.ibb.co/NpBsGrz/background.jpg",
+        charNow: "https://i.ibb.co/FXJJJ6J/Ephnel-Character-Art.png",
+};
+
